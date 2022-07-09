e570298d40cf35d189e0197a7cdaa3954cd9eb55
JANGHEE LEE <>
2022-07-09 22:41:36 +0900

    6-2 함수 인라인

---
+++ ch6/6-2-rf.js
@@ -0,0 +1,13 @@
+/* 6.2 함수 인라인하기 */
+// 예제 1
+export function rating(driver) {
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
+}
+
+// 예제 2
+function reportLines(customer) {
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
+}
