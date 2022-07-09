4706c63346cc41f7ed9ec61d9e81f97a096946c1
JANGHEE LEE <>
2022-07-09 22:58:35 +0900

    6-3 변수 추출하기

---
+++ ch6/6-3-1-rf.js
@@ -0,0 +1,8 @@
+/* 6.3 변수 추출하기 */
+//예제1
+export function price(order) {
+  const basePrice = order.quantity * order.itemPrice;
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  return basePrice - discount + shipping;
+}
